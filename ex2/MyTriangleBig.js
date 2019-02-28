10689d2b10064890e65cfed7b956eeb272688f34
Andre Mori <andremori16@gmail.com>
2019-02-28 15:39:49 +0000

    Ex2 added

---
+++ ex2/MyTriangleBig.js
@@ -0,0 +1,29 @@
+/**
+ * MyTriangleBig
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyTriangleBig extends CGFobject {
+    constructor(scene) {
+        super(scene);
+        this.initBuffers();
+    }
+    initBuffers() {
+        this.vertices = [
+            -2, 0, 0,	//0
+            2, 0, 0,	//1
+            0, 2, 0,	//2
+        ];
+
+        //Counter-clockwise reference of vertices
+        this.indices = [
+            0, 1, 2,
+            // inverter para aparecer do outro lado
+            2, 1, 0,
+        ];
+        
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    }
+}
+
