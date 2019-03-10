23412402c6bdce8df5775ba6eb414f360c6db574
DanielBradaschia <daniel.bradaschia@gmail.com>
2019-03-10 02:40:29 +0000

    TP2 done

--- ex2/MyTangram.js
+++ ex2/MyTangram.js
@@ -10,16 +10,13 @@ class MyTangram extends CGFobject {
         this.triangleSmall = new MyTriangleSmall(this.scene);
         this.parallelogram = new MyParallelogram(this.scene);
         this.diamond = new MyDiamond(this.scene);
-    }
-
-    convertAng(ang) {
-    return (Math.PI * ang / 180);
+        this.cube = new MyUnitCube(this.scene);
     }
 
     display(){
         // BLUE
         this.scene.pushMatrix();
-        this.scene.rotate(this.convertAng(180), 0, 0, 1);
+        this.scene.rotate(this.scene.convertAng(180), 0, 0, 1);
         this.scene.translate(0, -2, 0);
         this.triangleBig.display();
         this.scene.popMatrix();
@@ -27,38 +24,46 @@ class MyTangram extends CGFobject {
         this.scene.pushMatrix();
         this.scene.translate(1, 3, 0);
         this.scene.scale(Math.sqrt(2) / 2, Math.sqrt(2) / 2, Math.sqrt(2) / 2);
-        this.scene.rotate(this.convertAng(-135), 0, 0, 1);
+        this.scene.rotate(this.scene.convertAng(-135), 0, 0, 1);
         this.triangleBig.display();
         this.scene.popMatrix();
         // ORANGE
         this.scene.pushMatrix();
-        this.scene.rotate(this.convertAng(-135), 0, 0, 1);
+        this.scene.rotate(this.scene.convertAng(-135), 0, 0, 1);
         this.triangleBig.display();
         this.scene.popMatrix();
         // GREEN
         this.scene.pushMatrix();
         this.scene.translate(-0.3335, 2.9, 0);
-        this.scene.rotate(this.convertAng(20), 0, 0, 1);
+        this.scene.rotate(this.scene.convertAng(20), 0, 0, 1);
         this.diamond.display();
         this.scene.popMatrix();
         // YELLOW
         this.scene.pushMatrix();
         this.scene.translate(1.40, -1.40, 0);
         this.scene.scale(1, -1, 1);
-        this.scene.rotate(this.convertAng(135), 0, 0, 1);
+        this.scene.rotate(this.scene.convertAng(135), 0, 0, 1);
         this.parallelogram.display();
         this.scene.popMatrix();
         // RED
         this.scene.pushMatrix();
         this.scene.translate(-1.9, -1.9, 0);
-        this.scene.rotate(this.convertAng(45), 0, 0, 1);
+        this.scene.rotate(this.scene.convertAng(45), 0, 0, 1);
         this.triangleSmall.display();
         this.scene.popMatrix();
         // PURPLE
         this.scene.pushMatrix();
         this.scene.translate(-1.45, -3.5, 0);
-        this.scene.rotate(this.convertAng(45), 0, 0, 1);
+        this.scene.rotate(this.scene.convertAng(45), 0, 0, 1);
         this.triangleSmall.display();
+        this.scene.popMatrix();
+        // CUBE
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, -0.55);
+        this.scene.scale(8.5,8.5, 1);        
+        this.cube.display();
+        this.scene.popMatrix();        
+
     }
 }
 
