3016f98850c985380d15dfb4de269db365578af7
DanielBradaschia <daniel.bradaschia@gmail.com>
2019-04-04 22:21:35 +0100

    changed Mycylinder normals

--- WebCGF2/projA/MyCylinder.js
+++ WebCGF2/projA/MyCylinder.js
@@ -13,44 +13,40 @@ class MyCylinder extends CGFobject {
         this.vertices = [];
         this.indices = [];
         this.normals = [];
-
+        
         var alphaAng = 2 * Math.PI / this.slices;
         var step = 1 / this.stacks;
-
-        for (let i = 0; i < this.slices; i++) {
-
-            var sa = Math.sin(alphaAng * i);
-            var saa = Math.sin(alphaAng * (i + 1));
-            var ca = Math.cos(alphaAng * i);
-            var caa = Math.cos(alphaAng * (i + 1));
-
-            for (let j = 0; j < this.stacks; j++) {
-
+        
+        for (let i = 0; i <= this.slices; ++i) {
+                        
+            for (let j = 0; j <= this.stacks; ++j) {
+                
                 this.vertices.push(
-                    ca, (j + 1) * step, -sa,
-                    ca, j * step, -sa,
-                    caa, (j + 1) * step, -saa,
-                    caa, j * step, -saa,
+                    Math.cos(alphaAng * i), j * step, -Math.sin(alphaAng * i)
                 );
 
-
-                this.indices.push(
-                    4 * i * this.stacks + 4 * j + 0, 4 * i * this.stacks + 4 * j + 1, 4 * i * this.stacks + 4 * j + 2,
-                    4 * i * this.stacks + 4 * j + 3, 4 * i * this.stacks + 4 * j + 2, 4 * i * this.stacks + 4 * j + 1
-                );
+             /*   this.texCoords.push(
+                    i * 1 / this.slices, j * 1 / this.stacks
+                ); */
 
                 this.normals.push(
-                    Math.cos(alphaAng * i), 0, -Math.sin(alphaAng * i),
-                    Math.cos(alphaAng * i), 0, -Math.sin(alphaAng * i),
-                    0, 0, 0,
-                    0, 0, 0,
+                    Math.cos(alphaAng * i), 0, -Math.sin(alphaAng * i)
+                );
+           }
+        }
+
+        for (let i = 0; i < this.slices; ++i) {
+            for (let j = 0; j < this.stacks; ++j) {
+                this.indices.push(
+                    (i + 1) * (this.stacks + 1) + j, i * (this.stacks + 1) + j + 1, i * (this.stacks + 1) + j,
+                    i * (this.stacks + 1) + j + 1, (i + 1) * (this.stacks + 1) + j, (i + 1) * (this.stacks + 1) + j + 1
                 );
             }
         }
 
         this.primitiveType = this.scene.gl.TRIANGLES;
         this.initGLBuffers();
-    };
+    }
 
     updateBuffers(complexity) {
         this.slices = 3 + Math.round(9 * complexity); //complexity varies 0-1, so slices varies 3-12
